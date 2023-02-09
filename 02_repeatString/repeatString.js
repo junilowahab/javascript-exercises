186efb93f6c3ea7a26f2e9349fe3ca76cd9c297a
junilowahab <junilojoshuawahab@gmail.com>
2023-02-09 23:34:37 +0100

    SOlution for repeat string three times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let a = string;
+    for(let i = 1; i < num; i++){
+       string = a + string; 
+    };
+    return string;
 };
+
+repeatString('hey', 3);
 
 // Do not edit below this line
 module.exports = repeatString;
