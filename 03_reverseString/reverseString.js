e6625d703e73d6284a62b8061900f87f0c633e24
junilowahab <junilojoshuawahab@gmail.com>
2023-02-10 16:00:59 +0100

    Solution for 'reverses single word'

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringArray = string.split('');
+    let splitStringArray = stringArray.reverse();
+    let joinedSplitArray = splitStringArray.join('');
+    return joinedSplitArray;
 };
 
 // Do not edit below this line
