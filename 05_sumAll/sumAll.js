6af7bafb49ba2334c9741d5133ebb866850c6007
junilowahab <junilojoshuawahab@gmail.com>
2023-02-14 23:31:14 +0100

    Solution for 'sumAll.js'

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,35 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if(a < 0 || b < 0){
+        let error = 'ERROR'
+        return error;
+    };
+
+    if(typeof a !== 'number' || typeof b !== 'number'){
+        let error = 'ERROR'
+        return error;
+    }
+
+    if(a > b){
+        let sum = b;
+        for(let i = 1; i <= a; i++){
+            if(i > b){
+                sum = sum + i;
+            };
+        };
+    return sum;
+    };
+
+    if(a < b){
+        let sum = a;
+        for(let i = 1; i <= b; i++){
+            if(i > a){
+                sum = sum + i;
+            };
+        };
+    return sum;
+    };
+
+
 
 };
 
