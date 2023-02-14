a0cd5c477f4b4dd22ba59e8203f58be06853fb3d
junilowahab <junilojoshuawahab@gmail.com>
2023-02-14 13:42:08 +0100

    Solution for 'remove a single value'

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(givenArray, givenValue) {
+    //get the index of the value in the array
+    let selectedElementIndex = givenArray.indexOf(givenValue);
 
+    //delete/remove index from array
+    givenArray.splice(selectedElementIndex, 1);
+
+    //return new array
+    return givenArray;
 };
 
 // Do not edit below this line
