66f2c048da31966faed480f3eab79d34c8d3681a
junilowahab <junilojoshuawahab@gmail.com>
2023-02-16 14:37:32 +0100

    Solution for 'tempConversion'

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,22 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheit) {
+  let celsius = (5/9) * (farenheit - 32);
+  if(celsius - Math.floor(celsius) !== 0){
+    let roundedCelsius = celsius.toFixed(1);
+    return parseFloat(roundedCelsius);
+  }else{
+    return celsius;
+  }
+ 
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let farenheit = (celsius * (9/5)) + 32;
+  if(farenheit - Math.floor(farenheit) !== 0){
+    let roundedFarenheit = farenheit.toFixed(1)
+    return parseFloat(roundedFarenheit);
+  }else{
+    return farenheit;
+  }
 };
 
 // Do not edit below this line
